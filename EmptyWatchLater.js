613d68e0612184aed19135e0073d5df0ed766639
Nathemore Byyon <bmahoney.e@gmail.com>
2020-06-16 05:51:23 +0100

    create EmptyWatchLater.js

---
+++ EmptyWatchLater.js
@@ -0,0 +1,21 @@
+//delete all videos from watch later list
+//working version
+var qs = $$;
+var getVideo = () => qs('.ytd-playlist-video-list-renderer button')[0];
+
+function removeVideo()
+{
+    var video = getVideo();
+    if(video)
+    {
+        video.click();
+        setTimeout(function() {
+            qs('iron-dropdown span')[0].click();            
+            if(getVideo())
+            {
+                setTimeout(removeVideo, 100);
+            }        
+        }, 100);
+    }
+}
+removeVideo();
